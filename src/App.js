0e3d143aaff0fa6311dd798ada38b72135ee875b
LuckeSilver <jacks2kar@gmail.com>
2020-01-17 18:01:09 -0300

    inicio do front-end RadarDev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="inputBlock">           
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          
+          <div className="inputBlock">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="inputGroup">
+          <div className="inputBlock">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required/>
+          </div>
+          <div className="inputBlock">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+          </div>
+        <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul><li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37150645?s=460&v=4" alt="perfil-dev"/>
+              <div className="userInfo">
+                <strong>Lucas Silva</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+
+            <p>Sempre fui apaixonado por aprender coisas novas e obter conhecimento e ao ter contato com programação tive meus olhos abertos para novos horizontes.</p>
+            <a href="https://github.com/LuckeSilver">Acessar perfil no GitHub</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37150645?s=460&v=4" alt="perfil-dev"/>
+              <div className="userInfo">
+                <strong>Lucas Silva</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+
+            <p>Sempre fui apaixonado por aprender coisas novas e obter conhecimento e ao ter contato com programação tive meus olhos abertos para novos horizontes.</p>
+            <a href="https://github.com/LuckeSilver">Acessar perfil no GitHub</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37150645?s=460&v=4" alt="perfil-dev"/>
+              <div className="userInfo">
+                <strong>Lucas Silva</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+
+            <p>Sempre fui apaixonado por aprender coisas novas e obter conhecimento e ao ter contato com programação tive meus olhos abertos para novos horizontes.</p>
+            <a href="https://github.com/LuckeSilver">Acessar perfil no GitHub</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37150645?s=460&v=4" alt="perfil-dev"/>
+              <div className="userInfo">
+                <strong>Lucas Silva</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+
+            <p>Sempre fui apaixonado por aprender coisas novas e obter conhecimento e ao ter contato com programação tive meus olhos abertos para novos horizontes.</p>
+            <a href="https://github.com/LuckeSilver">Acessar perfil no GitHub</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37150645?s=460&v=4" alt="perfil-dev"/>
+              <div className="userInfo">
+                <strong>Lucas Silva</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+
+            <p>Sempre fui apaixonado por aprender coisas novas e obter conhecimento e ao ter contato com programação tive meus olhos abertos para novos horizontes.</p>
+            <a href="https://github.com/LuckeSilver">Acessar perfil no GitHub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
